c4bf84d3430eaf91d44dbf99072a6cb32cc55db2
Rupert_VJG <62993058+DevOps-Topics@users.noreply.github.com>
2022-12-26 16:21:06 +0530

    Update ec2.tf

--- Terraform-BasicsTO-Advanced/Basic_EC2/ec2.tf
+++ Terraform-BasicsTO-Advanced/Basic_EC2/ec2.tf
@@ -9,15 +9,7 @@ terraform {
 provider "aws" {
     region = "ap-south-1"
 }
-
-/* This local-exec provisioner allows you to run a command on the computer that you run terraform apply from
-locally after it has been created . The following command will create a file private ips.txt 
- with private ip of ec2 created */
-
 resource "aws_instance" "ngix-ec2" {
     ami = "ami-0cca134ec43cf708f"
     instance_type = "t2.micro"
-provisioner "local-exec" {
-    command = "echo ${aws_instance.ngix.ec2.private_ip}" > private_ips.txt
-}
 }
